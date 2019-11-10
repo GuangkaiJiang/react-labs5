cd9b8784f34ce079f9e92d611c593a0585ed0f76
Guangkai Jiang <skyrimtravel@outlook.com>
2019-11-10 22:57:37 +0100

    LAB5EX2(Clean up)

--- src/Player.js
+++ src/Player.js
@@ -4,22 +4,22 @@ import React from 'react'
 class Player extends React.Component{
     constructor(props) {
         super(props);
-        this.state = { nameOfPlayer1:props.nameOfPlayer ,
-            nameOfPlayer2: "chole" }
+        this.state = { 
+            
+         }
        
     }
   render(){
     return (
     <div style={{ borderStyle: 'solid', borderWidth: 5, margin: 20 }}> 
-        <h1>Player {this.props.number}</h1> 
+        <b>Player {this.props.number}</b> 
+        <br/>
         <>Name:{this.props.name}</>
         <br/>
         <>Played number of times:{this.props.times}</>
         <div style={{float: 'right'}}>
             <button>Play</button>
         </div>
-
-        <h1></h1>
     </div>
     )
   }
