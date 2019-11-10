a52c90ba932ee8ab3f0c3d8b32de4d92a10d2660
Guangkai Jiang <skyrimtravel@outlook.com>
2019-11-10 22:40:29 +0100

    LAB5EX2

--- src/GameAdmin.js
+++ src/GameAdmin.js
@@ -2,18 +2,34 @@ import React from 'react'
 import Player from './Player'
 
 class GameAdmin extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+        nameone:"",
+        nametwo:""
+    }
+    this.oneNameChangeEventHandler = this.oneNameChangeEventHandler.bind(this);
+    this.twoNameChangeEventHandler = this.twoNameChangeEventHandler.bind(this);
+}
+
+oneNameChangeEventHandler(event) {
+  this.setState({nameone: event.target.value});
+}
+twoNameChangeEventHandler(event) {
+  this.setState({nametwo: event.target.value});
+}
   render(){
     return (
     <div>
-        <Player number='one' name='<name from input field>' times='3'/>
-        <Player number='two' name='<name from input field>' times='4'/>
+        <Player number='one' name={this.state.nameone} times='3'/>
+        <Player number='two' name={this.state.nametwo} times='4'/>
         
         <div style={{ borderStyle: 'solid', borderWidth: 5, margin: 20 }}>
           <>Set Name of Player One: </>
-          <input type="string" />
+          <input type="text" value={this.state.nameone} onChange={this.oneNameChangeEventHandler}/>
           <br/>
           <>Set Name of Player Two: </>
-          <input type="string" />
+          <input type="text" value={this.state.nametwo} onChange={this.twoNameChangeEventHandler}  />
         </div>
     </div>
     )
